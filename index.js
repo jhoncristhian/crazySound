01865899cffbe8b0e8d9695480d38af3cae7ce2d
jhoncristhian <jhon.parco.27@unsch.edu.pe>
2024-09-10 23:07:02 -0500

    add changes and front, delete folder of files

--- index.js
+++ index.js
@@ -2,19 +2,124 @@ const express = require("express");
 const ytdl = require("youtube-dl-exec");
 const path = require("path");
 const fs = require("fs");
+// const { exec } = require("child_process");
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/music/:filename", (req, res) => {
+  const fileName = req.params.filename;
+  const filePath = path.join(__dirname, "music", fileName);
 
-app.get("/nameVideo", async (req, res) => {
+  // Establecer el encabezado Content-Disposition para forzar la descarga
+  res.setHeader("Content-Disposition", `attachment; filename=${fileName}`);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error("Error al descargar el archivo:", err);
+      res.status(404).send("Archivo no encontrado");
+    }
+  });
+});
+
+const folderPath = "./music";
+let deleteTimeout;
+
+const deleteFolderContents = (folderPath) => {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      const filePath = path.join(folderPath, file);
+      fs.unlink(filePath, (err) => {
+        if (err) throw err;
+      });
+    }
+    console.log(
+      `Todos los archivos en la carpeta ${folderPath} han sido eliminados.`
+    );
+  });
+};
+
+app.get("/save2", async (req, res) => {
   const videoUrl = req.query.url;
+
   if (!videoUrl) {
-    return res.status(400).send("No se proporcionó una URL de video.");
+    return res.status(400).json({
+      message: "No se proporcionó una URL de video.",
+      success: false,
+      statusCode: 400,
+    });
   }
 
+  let nameString = await nameSong(videoUrl);
+  console.log("NAME:: " + nameString);
+  // Guardar el archivo en la carpeta "music"
+  const outputFilePath = path.join(__dirname, "music", `${nameString}.m4a`);
+
+  try {
+    // Crear la carpeta "music" si no existe
+    const musicDir = path.join(__dirname, "music");
+    if (!fs.existsSync(musicDir)) {
+      fs.mkdirSync(musicDir);
+    }
+
+    // Ejecutar youtube-dl-exec para descargar solo el audio y guardarlo como mp3
+    await ytdl(videoUrl, {
+      extractAudio: true,
+      audioFormat: "m4a",
+      output: outputFilePath, // Guardar directamente en la ruta especificada
+    });
+    resetDeleteTimer(folderPath);
+    return res.json({
+      message: "El archivo de audio ha sido generado con éxito.",
+      success: true,
+      name: `/music/${nameString}.m4a`,
+      statusCode: 200,
+    });
+  } catch (err) {
+    console.error("Error inesperado:", err);
+    return res.status(500).json({
+      message: "Hubo un error inesperado.",
+      success: false,
+      statusCode: 500,
+    });
+  }
+});
+
+// app.get("/checkPython", (req, res) => {
+//   // Ejecuta el comando para obtener la versión de Python
+//   exec("python --version", (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`Error ejecutando el comando: ${error}`);
+//       return res.status(500).json({
+//         message:
+//           "Python no está instalado o no es accesible desde el servidor.",
+//         success: false,
+//         statusCode: 500,
+//       });
+//     }
+
+//     // Si Python está instalado, muestra la versión
+//     const pythonVersion = stderr || stdout; // Python muestra su versión en stderr a veces
+//     res.json({
+//       message: `Python está instalado. Versión: ${pythonVersion.trim()}`,
+//       success: true,
+//       statusCode: 200,
+//     });
+//   });
+// });
+
+app.get("/helloworld", (req, res) => {
+  res.json({
+    message: "Hello, World!'",
+    success: true,
+    statusCode: 200,
+  });
+});
+
+let nameSong = async (videoUrl) => {
   try {
     const info = await ytdl(videoUrl, {
       dumpSingleJson: true,
@@ -23,53 +128,33 @@ app.get("/nameVideo", async (req, res) => {
       noCheckCertificate: true,
     });
 
-    const videoTitle = info.title;
-    res.json({ title: videoTitle });
+    // Obtener el título del video
+    let title = info.title;
+
+    // Limpiar el título: solo permitir guiones, paréntesis y reemplazar espacios por guiones bajos
+    title = title
+      .replace(/[^\w\s()-]/g, "") // Eliminar caracteres no permitidos excepto letras, números, guiones, paréntesis
+      .replace(/\s+/g, "_") // Reemplazar espacios por guiones bajos
+      .trim(); // Eliminar espacios al principio y al final
+
+    return title;
   } catch (err) {
     console.error("Error al obtener el nombre del video:", err);
-    res.status(500).send("Error al obtener el nombre del video.");
+    throw err;
   }
-});
+};
 
-app.get("/download", async (req, res) => {
-  const videoUrl = req.query.url;
-  if (!videoUrl) {
-    return res.status(400).send("No se proporcionó una URL de video.");
+// Función para reiniciar el temporizador
+const resetDeleteTimer = (folderPath) => {
+  if (deleteTimeout) {
+    clearTimeout(deleteTimeout); // Cancelar cualquier temporizador previo
   }
 
-  let date = new Date();
-  let dateString = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(
-    2,
-    "0"
-  )}${String(date.getDate()).padStart(2, "0")}${String(
-    date.getHours()
-  ).padStart(2, "0")}${String(date.getMinutes()).padStart(2, "0")}`;
-  const outputFilePath = path.resolve(__dirname, dateString + ".mp3");
-
-  try {
-    await ytdl(videoUrl, {
-      output: outputFilePath,
-      extractAudio: true,
-      audioFormat: "mp3",
-    });
-
-    if (fs.existsSync(outputFilePath)) {
-      res.download(outputFilePath, "video.mp3", (err) => {
-        if (err) {
-          console.error("Error al enviar el archivo:", err);
-          res.status(500).send("Error al enviar el archivo.");
-        }
-        // Elimina el archivo después de enviarlo
-        fs.unlinkSync(outputFilePath);
-      });
-    } else {
-      res.status(500).send("1 Error al descargar o convertir el video.");
-    }
-  } catch (err) {
-    console.error("Error al descargar o convertir el video:", err);
-    res.status(500).send("2 Error al descargar o convertir el video.");
-  }
-});
+  // Establecer un nuevo temporizador para borrar la carpeta después de 15 minutos
+  deleteTimeout = setTimeout(() => {
+    deleteFolderContents(folderPath);
+  }, 1 * 60 * 1000); // 15 minutos
+};
 
 app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
--- public/js/script.js
+++ public/js/script.js
@@ -2,37 +2,57 @@ document
   .getElementById("download-form")
   .addEventListener("submit", async function (event) {
     event.preventDefault();
+
     const url = document.getElementById("url").value;
+    const convertButton = document.getElementById("convert-button");
+    const loadingSpinner = document.getElementById("loading");
+    const downloadButton = document.getElementById("download-button");
+
+    // Ocultar el botón de convertir y mostrar el círculo de carga
+    convertButton.style.display = "none";
+    loadingSpinner.style.display = "block";
 
     try {
+      // Enviar la solicitud para generar el archivo en el servidor
       const downloadResponse = await fetch(
-        `/download?url=${encodeURIComponent(url)}`
-      );
-      const videoNameResponse = await fetch(
-        `/nameVideo?url=${encodeURIComponent(url)}`
+        `/save2?url=${encodeURIComponent(url)}`
       );
 
       if (!downloadResponse.ok) {
-        throw new Error("Failed to download");
+        throw new Error("Failed to generate audio file");
       }
 
-      if (!videoNameResponse.ok) {
-        throw new Error("Failed to fetch video name");
+      // Obtener el JSON de la respuesta que contiene la URL de descarga
+      const responseData = await downloadResponse.json();
+
+      if (responseData.success) {
+        const downloadUrl = responseData.name; // URL relativa devuelta por el servidor
+
+        // Mostrar el botón de descarga y ocultar el círculo de carga
+        loadingSpinner.style.display = "none";
+        downloadButton.style.display = "block";
+
+        // Configurar la acción del botón de descarga
+        downloadButton.onclick = function () {
+          const a = document.createElement("a");
+          a.href = downloadUrl; // Usar la URL proporcionada por el servidor
+          a.download = `${downloadUrl.split("/").pop()}`; // Nombre del archivo
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+
+          // Recargar la página después de descargar
+          window.location.reload();
+        };
+      } else {
+        throw new Error(responseData.message || "Failed to download audio");
       }
-
-      const blob = await downloadResponse.blob();
-      const videoName = await videoNameResponse.json();
-
-      const downloadUrl = window.URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = downloadUrl;
-      a.download = `${videoName.title}.mp3`;
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(downloadUrl);
     } catch (error) {
       console.error(error);
       alert("Error downloading audio");
+
+      // Volver a mostrar el botón de convertir en caso de error
+      convertButton.style.display = "block";
+      loadingSpinner.style.display = "none";
     }
   });
